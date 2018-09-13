c24d563485c775fe09227ddcdd66ee515f183af9
DanielBaudry <daniel.baudry2@gmail.com>
2018-09-13 11:11:25 +0200

    [API] make signup work and show error msg with logger

--- routes/footprint.py
+++ routes/footprint.py
@@ -2,6 +2,7 @@
 from flask import current_app as app, jsonify, request
 from flask_login import current_user, login_required
 from engine.footprint import GetFootprint
+from models import User
 
 
 @app.route("/footprint/compute", methods=["POST"])
@@ -51,8 +52,40 @@ def save_footprint():
 def get_footprint():
     # For test purpose
     # current_user = User.query.filter_by(email='test@test.com').first()
+    #
+    # footprint = GetFootprint().execute(current_user)
 
-    footprint = GetFootprint().execute(current_user)
+    footprint = dict({"user_id": 1,
+                      "leaderbord":
+                          {
+                              "rank": 100,
+                              "user_id": 1,
+                              "total": 1000,
+                          },
+                      "statistics":
+                          {
+                              "total_carbon_saved": 15000,
+                              "percentage_earth_saved": 10,
+                          },
+                      "footprints": [
+                        {
+                            "id": 1,
+                            "footprint_type": "carbon",
+                            "footprint_value": 80
+                        },
+                        {
+                            "id": 2,
+                            "footprint_type": "waste",
+                            "footprint_value": 560
+                        },
+                        {
+                            "id": 3,
+                            "footprint_type": "water",
+                            "footprint_value": 1280
+                        }
+                    ]
+                    })
+
     return jsonify(footprint)
 
 
